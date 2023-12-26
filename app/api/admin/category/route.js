a037dceb398ac41497785b60d3dacfb9be7f4055
Marc <mgranier@comcast.net>
2023-12-26 17:37:25 -0500

    CreateList Catergoies API

--- app/api/admin/category/route.js
+++ app/api/admin/category/route.js
@@ -18,3 +18,13 @@ export async function POST(req) {
 		return NextResponse.json(err.message, { status: 500 });
 	}
 }
+
+export async function GET() {
+	await dbConnect;
+	try {
+		const categories = await Category.find({}).sort({ createdAt: -1 });
+		return NextResponse.json(categories);
+	} catch (err) {
+		return NextResponse.json(err.message, { status: 500 });
+	}
+}
