6b591e496c524b02f2af16042e9fb139724abadf
Marc <mgranier@comcast.net>
2023-12-26 17:33:14 -0500

    Create Category API

--- app/api/admin/category/route.js
+++ app/api/admin/category/route.js
@@ -4,23 +4,16 @@ import Category from '@/models/category';
 import slugify from 'slugify';
 
 export async function POST(req) {
-	await dbConnect();
-	const body = await req.json();
+	await dbConnect;
+	const body = req.json();
 	const { name } = body;
 
 	try {
-		const category = await Category.create({ name, slug: slugify(name) });
+		const category = await Category.create({
+			name,
+			slug: slugify(name),
+		});
 		return NextResponse.json(category);
-	} catch (err) {
-		return NextResponse.json(err._message, { status: 500 });
-	}
-}
-
-export async function GET() {
-	await dbConnect();
-	try {
-		const categories = await Category.find({}).sort({ createdAt: -1 });
-		return NextResponse.json(categories);
 	} catch (err) {
 		return NextResponse.json(err.message, { status: 500 });
 	}
